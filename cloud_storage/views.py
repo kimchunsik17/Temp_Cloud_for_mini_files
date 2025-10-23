56d4c1e96632420355dd7a6df915bdbbe8d051b8
이온규 <dydeod00@gmail.com>
2025-10-23 21:16:15 +0900

    feat: Add index view, template, and URL configuration

--- cloud_storage/views.py
+++ cloud_storage/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index_view(request):
+    # Logic for receiving and validating file ID and password will go here
+    return render(request, 'cloud_storage/index.html')
