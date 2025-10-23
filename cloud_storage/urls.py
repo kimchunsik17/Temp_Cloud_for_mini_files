c7e9d445467a614015d0ba3beaa6b4591c683285
이온규 <dydeod00@gmail.com>
2025-10-23 22:29:40 +0900

    feat: Add delete functionality to admin page and navigation buttons

--- cloud_storage/urls.py
+++ cloud_storage/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('upload/', views.upload_view, name='upload'),
     path('download/', views.download_view, name='download'),
     path('admin_page/', views.admin_page_view, name='admin_page'),
+    path('delete/<str:file_id>/', views.delete_file_view, name='delete_file'),
 ]
